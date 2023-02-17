8e55d343b436dba18b9dd40523b68890fd58cdef
Nemr Aslam <ma456@hw.ac.uk>
2023-02-17 10:45:24 +0400

    try fix

--- lib/app/services/feed/feed.service.g.dart
+++ lib/app/services/feed/feed.service.g.dart
@@ -50,7 +50,7 @@ class _FeedService implements FeedService {
     this._dio, {
     this.baseUrl,
   }) {
-    baseUrl ??= 'http://192.168.0.158:3000';
+    baseUrl ??= 'http://10.6.208.12:3000';
   }
 
   final Dio _dio;
